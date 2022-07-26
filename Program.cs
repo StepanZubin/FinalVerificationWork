77240e124ff9644476861f3b33109ca22c7609d5
STEPAN <chichetka27@gmail.com>
2022-07-26 20:32:41 +0400

    add PrintStringArray

--- Program.cs
+++ Program.cs
@@ -3,3 +3,21 @@ string[] arrayInput = new string[4] {"hello", "2", "world", ":-)"};
 string[] arrayOutput = new string[arrayInput.Length];
 int indexInput = 0;
 int indexOutput = 0; 
+
+
+PrintStringArray(arrayInput);
+
+void PrintStringArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
