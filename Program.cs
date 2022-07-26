d6c4ef844a71c4bfbeeff0fec0a8ac1c6889d671
STEPAN <chichetka27@gmail.com>
2022-07-26 20:29:01 +0400

    add data entry

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
 ﻿// array of strings
+string[] arrayInput = new string[4] {"hello", "2", "world", ":-)"};
+string[] arrayOutput = new string[arrayInput.Length];
+int indexInput = 0;
+int indexOutput = 0; 
